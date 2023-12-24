5b1c0acd2f17047ca4f620f0bef7357deba5fe78
Loic PETIT <8918083+loicpetit@users.noreply.github.com>
2023-12-23 21:06:00 -0500

    Add missing tests

--- main/input_test.go
+++ main/input_test.go
@@ -1,11 +1,195 @@
 package main
 
-import "testing"
+import (
+	"testing"
+	"time"
+)
 
 func TestInputString(t *testing.T) {
-	t.Fatal("TODO")
+	text := NewInput(
+		2,
+		map[int]*Creature{
+			1: NewCreature(1, 1, 1),
+			2: NewCreature(2, 2, 2),
+		},
+		NewPlayerState(5, map[int]*Drone{
+			3: NewDrone(3, 3, 3, 3, 3),
+			4: NewDrone(4, 4, 4, 4, 4),
+		}),
+		NewPlayerState(10, map[int]*Drone{
+			5: NewDrone(5, 5, 5, 5, 5),
+		}),
+	).String()
+	if text != "{nbCreatures:2, creatures:map[1:{id:1, color:1, type:1, coords:, scannedBy:[]} 2:{id:2, color:2, type:2, coords:, scannedBy:[]}], player:{score:5, drones:map[3:{id:3, x:3, y:3, emergency:3, battery:3} 4:{id:4, x:4, y:4, emergency:4, battery:4}]}, foe:{score:10, drones:map[5:{id:5, x:5, y:5, emergency:5, battery:5}]}}" {
+		t.Errorf("Unexpected text: %s", text)
+	}
 }
 
 func TestReaderRead(t *testing.T) {
-	t.Fatal("TODO")
+	stdin := NewStdinInterceptor()
+	stdin.Intercept()
+	go func() {
+		// turn 1
+		stdin.Write("2")      // nb creature
+		stdin.Write("2  0 0") // creature 0  (creature id, color, type)
+		stdin.Write("3  1 0") // creature 1  (creature id, color, type)
+		stdin.Write("0")      // my score
+		stdin.Write("0")      // foe score
+		stdin.Write("0")      // my scan count
+		// no creature
+		stdin.Write("0") // foe scan count
+		// no creature
+		stdin.Write("1")               // my drone count
+		stdin.Write("0 3333 500 0 30") // drone 0
+		stdin.Write("1")               // foe drone count
+		stdin.Write("1 6666 500 0 30") // drone 1
+		stdin.Write("0")               // drone scan count
+		// no scans
+		stdin.Write("2")                      // nb visibles creatures
+		stdin.Write("2  3467 3925 -141 141 ") // visible creature 0  (id, x, y, vx, vy)
+		stdin.Write("3  6532 3925 141  141 ") // visible creature 1  (id, x, y, vx, vy)
+		stdin.Write("2")                      // radar blip count
+		stdin.Write("0 2  BR")                // blip 0  (drone id, creature id, radar)
+		stdin.Write("0 3  BR")                // blip 1  (drone id, creature id, radar)
+		time.Sleep(1000 * time.Millisecond)
+		// turn 2
+		stdin.Write("0") // my score
+		stdin.Write("0") // foe score
+		stdin.Write("1") // my scan count
+		stdin.Write("2") // creature 2 scanned by player
+		stdin.Write("1") // foe scan count
+		stdin.Write("3") // creature 3 scanned by foe
+		// no creature
+		stdin.Write("1")               // my drone count
+		stdin.Write("0 3333 800 0 25") // drone 0
+		stdin.Write("1")               // foe drone count
+		stdin.Write("1 6666 800 0 25") // drone 1
+		stdin.Write("0")               // drone scan count
+		// no scans
+		stdin.Write("2")                      // nb visibles creatures
+		stdin.Write("2  3326 4066 -141 141 ") // visible creature 0  (id, x, y, vx, vy)
+		stdin.Write("3  6673 4066 141  141 ") // visible creature 1  (id, x, y, vx, vy)
+		stdin.Write("2")                      // radar blip count
+		stdin.Write("0 2  BL")                // blip 0  (drone id, creature id, radar)
+		stdin.Write("0 3  BR")                // blip 1  (drone id, creature id, radar)
+		time.Sleep(100 * time.Millisecond)
+	}()
+	inputs := NewReader().Read()
+	input1 := <-inputs
+	input2 := <-inputs
+	stdin.Close()
+	// assert
+	if input1.NbCreatures != 2 {
+		t.Errorf("Input 1 nb creatures should be 2 but is %d", input1.NbCreatures)
+	}
+	if len(input1.Creatures) != 2 {
+		t.Errorf("Input 1 nb creatures in map should be 2 but is %d", len(input1.Creatures))
+	} else {
+		if input1.Creatures[2].Id != 2 {
+			t.Errorf("Input 1 creature[2] should have id 2 but is %d", input1.Creatures[2].Id)
+		}
+		if input1.Creatures[2].Color != 0 {
+			t.Errorf("Input 1 creature[2] should have color 0 but is %d", input1.Creatures[2].Color)
+		}
+		if input1.Creatures[2].Type != 0 {
+			t.Errorf("Input 1 creature[2] should have type 0 but is %d", input1.Creatures[2].Type)
+		}
+		if input1.Creatures[2].Coords.X != 3467 {
+			t.Errorf("Input 1 creature[2] should have x 3467 but is %d", input1.Creatures[2].Coords.X)
+		}
+		if input1.Creatures[2].Coords.Y != 3925 {
+			t.Errorf("Input 1 creature[2] should have y 3925 but is %d", input1.Creatures[2].Coords.Y)
+		}
+		if input1.Creatures[2].Coords.Vx != -141 {
+			t.Errorf("Input 1 creature[2] should have vx -141 but is %d", input1.Creatures[2].Coords.Vx)
+		}
+		if input1.Creatures[2].Coords.Vy != 141 {
+			t.Errorf("Input 1 creature[2] should have vy 141 but is %d", input1.Creatures[2].Coords.Vy)
+		}
+		if len(input1.Creatures[2].scannedBy) != 0 {
+			t.Errorf("Input 1 creature[2] should have nb scanned by 0 but is %d", len(input1.Creatures[2].scannedBy))
+		}
+		if input1.Creatures[3].Id != 3 {
+			t.Errorf("Input 1 creature[3] should have id 3 but is %d", input1.Creatures[3].Id)
+		}
+		if input1.Creatures[3].Color != 1 {
+			t.Errorf("Input 1 creature[3] should have color 1 but is %d", input1.Creatures[3].Color)
+		}
+		if input1.Creatures[3].Type != 0 {
+			t.Errorf("Input 1 creature[3] should have type 0 but is %d", input1.Creatures[3].Type)
+		}
+		if input1.Creatures[3].Coords.X != 6532 {
+			t.Errorf("Input 1 creature[3] should have x 6532 but is %d", input1.Creatures[3].Coords.X)
+		}
+		if input1.Creatures[3].Coords.Y != 3925 {
+			t.Errorf("Input 1 creature[3] should have y 3925 but is %d", input1.Creatures[3].Coords.Y)
+		}
+		if input1.Creatures[3].Coords.Vx != 141 {
+			t.Errorf("Input 1 creature[3] should have vx 141 but is %d", input1.Creatures[3].Coords.Vx)
+		}
+		if input1.Creatures[3].Coords.Vy != 141 {
+			t.Errorf("Input 1 creature[3] should have vy 141 but is %d", input1.Creatures[3].Coords.Vy)
+		}
+		if len(input1.Creatures[3].scannedBy) != 0 {
+			t.Errorf("Input 1 creature[3] should have nb scanned by 0 but is %d", len(input1.Creatures[3].scannedBy))
+		}
+	}
+	if input2.NbCreatures != 2 {
+		t.Errorf("Input 2 nb creatures should be 2 but is %d", input2.NbCreatures)
+	} else {
+		if input2.Creatures[2].Id != 2 {
+			t.Errorf("Input 2 creature[2] should have id 2 but is %d", input2.Creatures[2].Id)
+		}
+		if input2.Creatures[2].Color != 0 {
+			t.Errorf("Input 2 creature[2] should have color 0 but is %d", input2.Creatures[2].Color)
+		}
+		if input2.Creatures[2].Type != 0 {
+			t.Errorf("Input 2 creature[2] should have type 0 but is %d", input2.Creatures[2].Type)
+		}
+		if input2.Creatures[2].Coords.X != 3326 {
+			t.Errorf("Input 2 creature[2] should have x 3326 but is %d", input2.Creatures[2].Coords.X)
+		}
+		if input2.Creatures[2].Coords.Y != 4066 {
+			t.Errorf("Input 2 creature[2] should have y 4066 but is %d", input2.Creatures[2].Coords.Y)
+		}
+		if input2.Creatures[2].Coords.Vx != -141 {
+			t.Errorf("Input 2 creature[2] should have vx -141 but is %d", input2.Creatures[2].Coords.Vx)
+		}
+		if input2.Creatures[2].Coords.Vy != 141 {
+			t.Errorf("Input 2 creature[2] should have vy 141 but is %d", input2.Creatures[2].Coords.Vy)
+		}
+		if len(input2.Creatures[2].scannedBy) != 1 {
+			t.Errorf("Input 2 creature[2] should have nb scanned by 1 but is %d", len(input2.Creatures[2].scannedBy))
+		}
+		if input2.Creatures[2].scannedBy[0] != "player" {
+			t.Errorf("Input 2 creature[2] should be scanned by player but is %s", input2.Creatures[2].scannedBy[0])
+		}
+		if input2.Creatures[3].Id != 3 {
+			t.Errorf("Input 2 creature[3] should have id 3 but is %d", input2.Creatures[3].Id)
+		}
+		if input2.Creatures[3].Color != 1 {
+			t.Errorf("Input 2 creature[3] should have color 1 but is %d", input2.Creatures[3].Color)
+		}
+		if input2.Creatures[3].Type != 0 {
+			t.Errorf("Input 2 creature[3] should have type 0 but is %d", input2.Creatures[3].Type)
+		}
+		if input2.Creatures[3].Coords.X != 6673 {
+			t.Errorf("Input 2 creature[3] should have x 6673 but is %d", input2.Creatures[3].Coords.X)
+		}
+		if input2.Creatures[3].Coords.Y != 4066 {
+			t.Errorf("Input 2 creature[3] should have y 4066 but is %d", input2.Creatures[3].Coords.Y)
+		}
+		if input2.Creatures[3].Coords.Vx != 141 {
+			t.Errorf("Input 2 creature[3] should have vx 141 but is %d", input2.Creatures[3].Coords.Vx)
+		}
+		if input2.Creatures[3].Coords.Vy != 141 {
+			t.Errorf("Input 2 creature[3] should have vy 141 but is %d", input2.Creatures[3].Coords.Vy)
+		}
+		if len(input2.Creatures[3].scannedBy) != 1 {
+			t.Errorf("Input 2 creature[3] should have nb scanned by 1 but is %d", len(input2.Creatures[3].scannedBy))
+		}
+		if input2.Creatures[3].scannedBy[0] != "foe" {
+			t.Errorf("Input 2 creature[3] should be scanned by foe but is %s", input2.Creatures[3].scannedBy[0])
+		}
+	}
 }
--- main/state_test.go
+++ main/state_test.go
@@ -26,5 +26,68 @@ func TestStateString(t *testing.T) {
 }
 
 func TestStateSetters(t *testing.T) {
-	t.Fatal("TODO")
+	state1 := NewState()
+	state2 := state1.SetCreatures(2, map[int]*Creature{
+		1: NewCreature(1, 1, 1),
+		2: NewCreature(2, 2, 2),
+	})
+	state3 := state2.
+		SetPlayer(NewPlayerState(5, map[int]*Drone{
+			3: NewDrone(3, 3, 3, 3, 3),
+			4: NewDrone(4, 4, 4, 4, 4),
+		})).
+		SetFoe(NewPlayerState(10, map[int]*Drone{
+			5: NewDrone(5, 5, 5, 5, 5),
+		}))
+	if state1 == state2 {
+		t.Error("State 1 should NOT be the same as state 2")
+	}
+	if state1 == state3 {
+		t.Error("State 1 should NOT be the same as state 3")
+	}
+	if state2 == state3 {
+		t.Error("State 2 should NOT be the same as state 3")
+	}
+	if state1.NbCreatures != 0 {
+		t.Errorf("State 1 should have 0 creatures but has %d", state1.NbCreatures)
+	}
+	if len(state1.Creatures) != 0 {
+		t.Errorf("State 1 should have 0 creatures in map but has %d", len(state1.Creatures))
+	}
+	if state1.Player != nil {
+		t.Error("State 1 should NOT have a player")
+	}
+	if state1.Foe != nil {
+		t.Error("State 1 should NOT have a foe")
+	}
+	if state2.NbCreatures != 2 {
+		t.Errorf("State 2 should have 2 creatures but has %d", state2.NbCreatures)
+	}
+	if len(state2.Creatures) != 2 {
+		t.Errorf("State 2 should have 2 creatures in map but has %d", len(state2.Creatures))
+	}
+	if state2.Player != nil {
+		t.Error("State 2 should NOT have a player")
+	}
+	if state2.Foe != nil {
+		t.Error("State 2 should NOT have a foe")
+	}
+	if state3.NbCreatures != 2 {
+		t.Errorf("State 3 should have 2 creatures but has %d", state3.NbCreatures)
+	}
+	if len(state3.Creatures) != 2 {
+		t.Errorf("State 3 should have 2 creatures in map but has %d", len(state3.Creatures))
+	}
+	if state3.Creatures[0] != state2.Creatures[0] {
+		t.Error("State 3 creature 0 should be same as in state 2")
+	}
+	if state3.Creatures[1] != state2.Creatures[1] {
+		t.Error("State 3 creature 1 should be same as in state 2")
+	}
+	if state3.Player == nil {
+		t.Error("State 3 should have a player")
+	}
+	if state3.Foe == nil {
+		t.Error("State 3 should have a foe")
+	}
 }
